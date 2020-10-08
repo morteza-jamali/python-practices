44fd2c0ea9405ebce4321d1077ad1d7429c8befe
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-08 21:56:26 +0330

    Get area of Parallelogram

---
+++ p1.py
@@ -0,0 +1,5 @@
+base = float(input("Get length of base ?"))
+height = float(input("Get length of height ?"))
+area = base * height
+print("This is area" , area)
+input("Please hit Enter to exit program ...")
