158114f90123d5270675de64272eb3b62453fa42
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-09 11:04:50 +0330

    Luke, I Am Your ... practice

---
+++ p4.py
@@ -0,0 +1,16 @@
+relations = {
+    "Darth Vader" : "father" ,
+    "Leia" : "sister" ,
+    "Han" : "brother in law" ,
+    "R2D2" : "droid"
+}
+
+def relation_to_luke(name):
+    if name in relations:
+        return "Luke, I am your %s." % relations[name]
+    else:
+        return "I don't khow who you are !"
+
+input_value = input("Get your name ?\n")
+result = relation_to_luke(input_value)
+print(result)
