447ffc3476cfe44afd67635b0cb27e7f1e70521e
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-12 06:41:49 +0330

    Get Student Names

---
+++ p8.py
@@ -0,0 +1,12 @@
+# Source List : https://edabit.com/challenge/HvkPdhijquecKASdF
+# Level : Easy
+
+def get_student_names(dict):
+    names = []
+
+    for name in dict.values():
+        names.append(name)
+    
+    names.sort()
+    
+    return names
