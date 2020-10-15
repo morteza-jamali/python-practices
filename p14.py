a589ac9da8507b726b85fcaebb702805636157a7
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-15 07:20:59 +0330

    A Circle and Two Squares

---
+++ p14.py
@@ -0,0 +1,8 @@
+# Source Link : https://edabit.com/challenge/NNhkGocuPMcryW7GP
+# Level : Medium
+
+def square_areas_difference(rad):
+  bigger_area = 4 * rad ** 2
+  smaller_area = 2 * rad ** 2
+
+  return bigger_area - smaller_area
