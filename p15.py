087703c88ccc0c97d313c4974ece2d79ce1f929a
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-21 09:03:05 +0330

    Simple OOP Calculator

---
+++ p15.py
@@ -0,0 +1,12 @@
+# Source Link : https://edabit.com/challenge/ta8GBizBNbRGo5iC6
+# Level : Medium
+
+class Calculator:
+  def add(self , n1 , n2):
+    return n1 + n2
+  def subtract(self , n1 , n2):
+    return n1 - n2
+  def multiply(self , n1 , n2):
+    return n1 * n2
+  def divide(self , n1 , n2):
+    return n1 // n2
